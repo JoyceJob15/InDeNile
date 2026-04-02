a1f4549f333e63eeb970d1cb998cc4a8009796de
JoyceJob15 <joycemaryjob@gmail.com>
2026-04-02 02:34:26 -0400

    scene 1.1

---
+++ Assets/_ConestogaMultiplayer/Scripts/SceneTrigger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTrigger : MonoBehaviour
+{
+    public void TestTrigger()
+    {
+        Debug.Log("Timeline Triggered!");
+    }
+}
